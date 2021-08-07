3b6ab8ddb57d295986263498ed65295391e15b66
Mimoune <djouallah@users.noreply.github.com>
2021-08-07 22:10:35 +1000

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import streamlit as st
 import base64
 import altair as alt
 col1, col2, col3 = st.beta_columns(3)
-@st.cache(suppress_st_warning=True)
+@st.cache(allow_output_mutation=True)
 def Run_Query(DAX_Query2):
       Query_text='{ "queries": [{"query":'+DAX_Query2+'}], "serializerSettings":{"incudeNulls": true}}'
       api_out = requests.post(url=url_Query,data=Query_text, headers=header)
