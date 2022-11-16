bd334abea06ae00b364a09c7412babf98f32a5ca
Mimoune <djouallah@users.noreply.github.com>
2022-11-16 17:58:12 +1000

    Update app.py

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ def convert_df(df):
             return df.to_csv().encode('utf-8')
 
 csv = convert_df(dd)
-col1.download_button(
+col3.download_button(
             label="Download data as CSV",
             data=csv,
             file_name='large_df.csv',
